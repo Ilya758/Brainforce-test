93d8bc20d84682f73e5c7873f3b4c866604d3f99
Ilya758 <subscriber6436@gmail.com>
2021-06-19 16:22:23 +0300

    complete check mask and form submit

--- js/script.js
+++ js/script.js
@@ -57,3 +57,57 @@ let phone = $('#form__phone');
 phone.mask('+37599 999-99-99');
 
 
+name.keyup((evt) => {
+    check(evt, 12, name, $('#name_error'));
+});
+phone.keyup((evt) => {
+    check(evt, 16, phone, $('#tel_error'));
+});
+
+$('#button_complete').click((event) => {
+    event.preventDefault()
+    if (name.val().length < 12 || phone.val().length != 16) {
+        $('#button_complete').removeClass('button_v_press')
+        event.preventDefault();
+        check(event, 12, name, $('#name_error'));
+        check(event, 16, phone, $('#tel_error'));
+    }
+    else {
+        $('#popup__complete').addClass(['form_v_active', 'form_s_enabled']);
+        $('#submit_btn').click(() => {
+            $('body').css({
+                'animation': 'sweep 4s linear'
+            })
+            setTimeout(() => {
+                $('#form').submit();
+            }, 1800);
+        })
+    }
+})
+
+function check(evt, chars, elem, id) {
+    evt.preventDefault();
+    let target = $(evt.target);
+    let length = target.val().length;
+    if (length == 0) {
+        evt.preventDefault();
+        elem.addClass('input_v_invalid');
+        id.addClass('text_v_invalid');
+        id.removeClass('text_v_invalid_green');
+        id.html("Обязательное поле");
+
+    } else if (length < chars) {
+        evt.preventDefault();
+        elem.removeClass('input_v_invalid');
+        id.removeClass('text_v_invalid');
+        id.addClass('text_v_invalid_green');
+        if (length == (chars - 1)) id.html("Остался 1 знак");
+        else if (length == (chars - 4) || length == (chars - 3) || length == (chars - 2)) id.html(`Осталось ${(chars - length)} знака`);
+        else id.html(`Осталось ${(chars - length)} знаков`);
+
+    } else {
+        evt.preventDefault()
+        id.removeClass('text_v_invalid_green');
+    }
+}
+
